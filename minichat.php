67c277947a3238a9ae4318a20555ef4e192b58e0
sanouo <sanouo591@gmail.com>
2017-09-14 11:20:34 +0200

    create minichat

---
+++ minichat.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mini-Chat</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="icon" href="favi.ico">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+  </head>
+  <body>
+
+    <header>
+      <h1>Mini-Chat</h1>
+    </header>
+
+
+    <form class="formu" action="minichat_post.php" method="post">
+      <div class="form-group">
+        <input type="text" class="form-control" id="formGroupExampleInput" name="pseudo"  value="<?php echo $_COOKIE['pseudo']; ?>">
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" id="formGroupExampleInput2" name="message" placeholder="message">
+      </div>
+      <button type="submit" value="Envoyer" class="btn btn-primary">Envoyer</button>
+    </form>
+
+
+<section class="info">
+
+
+    <?php
+    try
+    {
+    $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'phpmyadmin', 'sana15');
+    }
+    catch (Exception $e)
+    {
+        die('Erreur : ' . $e->getMessage());
+    }
+
+    $reponse = $bdd->query('SELECT * FROM minichat ORDER BY id DESC LIMIT 0, 10');
+
+    while ($donnees = $reponse->fetch())
+    {
+    ?>
+
+    <?php echo $donnees['pseudo']; ?> :
+    <?php echo $donnees['message']; ?><br>
+    <?php
+}
+$reponse->closeCursor();
+?>
+
+</section>
+
+
+  </body>
+</html>
---
+++ minichat_post.php
@@ -0,0 +1,22 @@
+<?php
+setcookie('pseudo',$_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
+
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'phpmyadmin', 'sana15');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+$req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(:pseudo, :message)');
+
+$req->execute(array(
+    'pseudo' => $_POST['pseudo'],
+    'message' => $_POST['message']
+    ));
+
+header('Location: minichat.php');
+ ?>
